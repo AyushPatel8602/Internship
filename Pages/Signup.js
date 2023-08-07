434028a80b8bb2140ac8a55de92946996ebe8008
AyushPatel8602 <patelayush8602@gmail.com>
2023-08-07 12:27:45 +0530

    first commit

---
+++ Pages/Signup.js
@@ -0,0 +1,15 @@
+const reset = ()=>{
+    const fname = document.getElementById("fname");
+    const lnmae = document.getElementById("lname");
+    const pnum = document.getElementById("pnum");
+    const email = document.getElementById("email");
+    const pass = document.getElementById("pass");
+    const cpass = document.getElementById("cpass");
+
+    fname.value = "";
+    lnmae.value = "";
+    pnum.value = "";
+    email.value = "";
+    pass.value = "";
+    cpass.value = "";
+}
